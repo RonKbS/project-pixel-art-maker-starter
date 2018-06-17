72ea698393a68fd9cf907d55530acd8f86ff363f
RonKbS <stephen.bamwite@gmail.com>
2018-06-17 11:07:19 +0300

    Use toggle method Only removes cells so far

--- designs.js
+++ designs.js
@@ -2,18 +2,6 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-/*$(function(){
-    $('h2').addClass('yata');
-});*/
-
-/*$ (function makeGrid(height, length){
-    for (i = 0; i < height; i++){
-        $ ('#pixelCanvas').append('<tr></tr>');
-        for (i = 0; i < length; i++){
-            $('tr').append('<td></td>');
-        }
-    }
-});*/
 $(document).ready(function (){
     $('#sizePicker').on('submit',function makeGrid() {
         event.preventDefault();
@@ -30,11 +18,13 @@ $(document).ready(function (){
     });
      
     $('#pixelCanvas').on('click','td',function () {
-        //$(this).clear();
+        $(this).toggle(function(){
+        $(this).css('background-color', '');
         var paint = $('#colorPicker').val();
         $(this).css('background-color', paint);
+        });
       });
-    });
+    });//document ready
    /*
   $(document).ready(function (){
     $('#sizePicker').on('submit', function makeGrid() {
