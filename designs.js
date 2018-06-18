547143423ea4bc0d7051636e82227043983d1da0
RonKbS <stephen.bamwite@gmail.com>
2018-06-18 23:43:11 +0300

    Add distinct classes to each td cell so far have 0s on all Also now using local jquery file

--- designs.js
+++ designs.js
@@ -1,7 +1,8 @@
 // Select color input
 // Select size input
+//http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
 
-// When size is submitted by the user, call makeGrid()
+
 $(document).ready(function (){
     $('#sizePicker').on('submit',function makeGrid() {
         event.preventDefault();
@@ -9,39 +10,31 @@ $(document).ready(function (){
         var height = $('#inputHeight').val();
         var length = $('#inputWeight').val();
 
+        
         for (var i = 0; i < height; i++){
             $('#pixelCanvas').append('<tr></tr>');
         }
+        
         for (var j = 0; j < length; j++){
-            $('tr').append('<td class="cells"></td>');
+            var c = '<td></td>'
+            $('tr').append(c);
         }
+        k = 0;
+        $('td').each(function(){
+            $(this).addClass(k.toString());
+        });
+        /*for (var k = 0; k < lenght * width; k++){...was going for each cell...class=cells'+ (j).toString() + '
+            $('td').addClass(k.toString());
+        }*/
     });
      
-    $('#pixelCanvas').on('click','td',function () {
-        $(this).toggle(function(){
-        $(this).css('background-color', '');
-        var paint = $('#colorPicker').val();
-        $(this).css('background-color', paint);
-        });
+    /*$('#pixelCanvas').on('click','td',function () {
+        $(this).css('background-color', $('#colorPicker').val());
+      },
+    function(){
+        $(this).toggleClass('.cells');
+    });*/
+    $('#pixelCanvas').on('click','.cells', function () {
+        $(this).css('background-color',$('#colorPicker').val());
       });
     });//document ready
-   /*
-  $(document).ready(function (){
-    $('#sizePicker').on('submit', function makeGrid() {
-        event.preventDefault();
-        $('#pixelCanvas').empty();
-        var height = $('#inputHeight').val();
-        var length = $('#inputWeight').val();
-
-        for (var i = 0; i < height; i++){
-            $('#pixelCanvas').append('<tr></tr>');
-        }
-        for (var j = 0; j < length; j++){
-            $('tr').append('<td class="cells"></td>');
-        }
-    });
-	$('#pixelCanvas').on('click','td', function () {
-        var paint = $('#colorPicker').val();
-        $(this).css('background-color', paint);
-      });
-	  });*/
